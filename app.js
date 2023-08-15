28c1ccd5d974d2caa0ee442571dc295c698f84d1
Cássio Tenório <cassiotenoriosc@gmail.com>
2023-08-15 11:58:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const imgDefault = './default_image.png';
 
 async function buscaFilme(nomeBusca) {
   const resposta = await fetch(
-    `http://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`
+    `https://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`
   );
   return resposta.json();
 }
